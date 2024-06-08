867e1cdc9379281879dac9d3660153ea207f4758
Matt Todd <matt.todd@cfa.vic.gov.au>
2024-06-08 16:30:53 +0930

    test

--- health.js
+++ health.js
@@ -98,8 +98,10 @@ async function healthCheck() {
     port.write('AT+CMGF=1\r'); // set SMS text mode
     port.write(`AT+CMGS="${process.env.HEALTH_CHECK}"\r`); // send sms message
     port.write('HealthCheck\r\n');
-    port.write('\x1A');
+    port.write('0x1a');
     port.write('^z'); 
+
+    console.log('HealthCheck sent');
   });
 
   // try {
